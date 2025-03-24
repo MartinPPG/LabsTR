36733d4a718081f7c7d2dca28817d553bbc72967
MartinPPG <112394127+MartinPPG@users.noreply.github.com>
2025-03-24 20:17:56 +0300

    Update factorial.cpp

--- factorial.cpp
+++ factorial.cpp
@@ -1,5 +1,3 @@
-
-
 #include <iostream>
 #include <cstdlib> // Для преобразования строки в число
 
@@ -7,30 +5,30 @@ using namespace std;
 
 // Функция для вычисления факториала
 long long factorial(int n) {
-if (n < 0) {
-cerr << "Ошибка: факториал отрицательного числа не определён." << endl;
-return -1;
-}
-long long result = 1;
-for (int i = 1; i <= n; ++i) {
-result *= i;
-}
-return result;
+    if (n < 0) {
+        return -1; // Возвращаем -1 вместо сообщения об ошибке
+    }
+    long long result = 1;
+    for (int i = 1; i <= n; ++i) {
+        result *= i;
+    }
+    return result;
 }
 
 int main(int argc, char* argv[]) {
-if (argc != 2) {
-cout << "Ошибка: требуется один аргумент (число для вычисления факториала)." << endl;
-return 1;
+    if (argc != 2) {
+        cout << "Ошибка: требуется один аргумент (число для вычисления факториала)." << endl;
+        return 1;
+    }
+
+    int n = atoi(argv[1]); // Преобразуем строку в число
+
+    long long result = factorial(n);
+    if (result != -1) {
+        cout << "Факториал " << n << " = " << result << endl;
+    } else {
+        cout << "Ошибка: факториал отрицательного числа не определён." << endl;
+    }
+
+    return 0;
 }
-
-int n = atoi(argv[1]); // Преобразуем строку в число
-
-long long result = factorial(n);
-if (result != -1) {
-cout << "Факториал " << n << " = " << result << endl;
-}
-
-return 0;
-}
-
